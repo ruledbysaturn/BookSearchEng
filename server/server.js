3d412c860607ce419efd0c2adcd9c993d93a56da
Ruminate <inramiland@gmail.com>
2024-02-03 13:47:00 -0500

    updated server file with apollo server, created schema folder and required files within, imported typedefs/resolvers to server file

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
+const { typeDefs, resolvers } = require('./schemas');
+
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
-const {authMiddleware} = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,7 +13,13 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use('/api/*', authMiddleware);
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+server.applyMiddleware({ app, path: '/graphql' });
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
