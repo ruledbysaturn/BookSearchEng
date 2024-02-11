0653fd308032be04865b507a2fdda443fdfd0421
Ruminate <inramiland@gmail.com>
2024-02-10 20:22:41 -0500

    index

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,11 +1,6 @@
 const router = require('express').Router();
-const path = require('path');
-const apiRoutes = require('./api');
+const userRoutes = require('./user-routes');
 
 router.use('/users', userRoutes);
-
-router.use((req, res) => {
-    res.sendFile(path.join(__dirname, '../../client/build/index.html'));
-});
 
 module.exports = router;
